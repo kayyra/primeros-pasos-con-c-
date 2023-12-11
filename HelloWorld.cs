01d97af0141a1bfb4f3d76cb43b7ad9ee976e925
Ana <anarodriguez.p@hotmail.es>
2023-12-11 16:20:53 +0100

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpHelloWorld
+{
+    class HelloWorld 
+    {
+         static void Main(string[] args)
+         {
+            // Hola Mundo
+            Console.WriteLine("Hello, C#!");
+             /*
+             Esto 
+             es 
+             un
+             comentario
+             */
+                // Declaramos e incializamos la variable myString con un valor inicial
+             string myString = "Esto es una cadena de texto";
+
+            //  Aquí cambiamos el valor de la variable myString
+             myString = "Aquí cambio el valor de la cadena de texto";
+
+            //  Aquí imprimimos el valor actualizado en la consola
+             Console.WriteLine(myString);
+         }
+    }
+}
+
+
+
+
